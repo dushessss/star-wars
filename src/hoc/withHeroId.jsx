f54267289545844d77f62dc03ef6fd7b3b93cc22
Andrew <cheremisin2@gmail.com>
2024-04-27 14:28:06 +0300

    fix routing

--- src/hoc/withHeroId.jsx
+++ src/hoc/withHeroId.jsx
@@ -11,7 +11,7 @@ export const withHeroId =(route) => (Component) => (props)=>{
 
     useEffect(()=>{
         if(!Object.keys(characters).includes(heroId)){
-            navigate(`/${route}/${defaultHero}`);
+            navigate(`/star-wars/${route}/${defaultHero}`);
         }else {
             changeMainHero(heroId);
         }
